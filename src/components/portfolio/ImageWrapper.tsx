7ad23e86e1b4533c4f64055954224aa7118f53c8
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-02 20:32:55 +0200

    Changed layout to no longer use wrapper for double images. Added a single  container to display all.

---
+++ src/components/portfolio/ImageWrapper.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledImageWrapper = styled.div``;
+
+const ImageWrapper = () => {
+    return <div></div>;
+};
+
+export default ImageWrapper;
---
+++ src/components/portfolio/ImagesContainer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import Photo from "./Photo";
+
+const StyledImagesContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+    width: 85%;
+`;
+
+interface Props {
+    images: { src: string; id: number }[];
+}
+
+const ImagesContainer = (props: Props) => {
+    return (
+        <StyledImagesContainer>
+            {props.images.map(img => {
+                return img != props.images[0] && <Photo key={img.id} img={img} />;
+            })}
+        </StyledImagesContainer>
+    );
+};
+
+export default ImagesContainer;
