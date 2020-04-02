094a706d1e500bd389e70c60f921767562e6f7d2
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-02 22:24:03 +0200

    added margin to the cards

--- src/components/main-site/PortfolioCard.tsx
+++ src/components/main-site/PortfolioCard.tsx
@@ -5,6 +5,7 @@ import CustomLink from "../navigation/CustomLink";
 
 const StyledCardContainer = styled.div`
     width: 30rem;
+    margin: 1rem;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 `;
 
