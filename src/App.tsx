ac5d8dddb3f3489779ba565b054845ad698e72b8
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-02 20:35:56 +0200

    Removed unused imports and added props for the projectpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import LandingPage from "./components/main-site/LandingPage";
 import NavBar from "./components/navigation/Navbar";
-import styled from "styled-components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ProjectPage from "./components/portfolio/ProjectPage";
 import SlideNumberContextProvider from "./components/context-providers/SlideNumberContextProvider";
 import ViewerStatusProvider from "./components/context-providers/ViewerStatusProvider";
+import imageLoader, { getDescription } from "./static/util/baltazarImgLoader";
 
 function App() {
     return (
@@ -15,7 +15,10 @@ function App() {
                 <Route path="/" exact component={LandingPage} />
                 <ViewerStatusProvider>
                     <SlideNumberContextProvider>
-                        <Route path="/portfolio/baltazár" exact component={ProjectPage} />
+                        <Route
+                            path="/portfolio/baltazár"
+                            render={props => <ProjectPage images={imageLoader()} description={getDescription()} />}
+                        />
                     </SlideNumberContextProvider>
                 </ViewerStatusProvider>
             </Switch>
