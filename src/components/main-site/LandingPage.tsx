1ebdd4f9ad77c60495ad839001610f7bcb609ea4
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-02 21:06:26 +0200

    Site now scrolls to portfolios even if coming from a different route when link is clicked.

--- src/components/main-site/LandingPage.tsx
+++ src/components/main-site/LandingPage.tsx
@@ -1,14 +1,14 @@
-import React, { useState, useRef, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import styled from "styled-components";
 import imageLoader from "../../static/util/baltazarImgLoader";
 import Hero from "./Hero";
 import Photo from "../portfolio/Photo";
 import CoverPhoto from "./CoverPhoto";
 import SectionTitle from "./SectionTitle";
-import { ScrollRefContext } from "../context-providers/ScrollRefContextProvider";
-import { Link } from "react-scroll";
+import { scroller as scroll, scroller } from "react-scroll";
 import ImageCard from "./PortfolioCard";
 import CardImage from "./CardImage";
+import { NavigationContext } from "../context-providers/NavigationContextProvider";
 
 interface HeroProps {
     photo?: string;
@@ -25,6 +25,16 @@ const LandingPage = () => {
     const photoGallery = imageLoader();
 
     const randomPhoto: { id: number; src: string } = photoGallery[Math.floor(Math.random() * photoGallery.length)];
+
+    const [link, setLink] = useContext(NavigationContext);
+
+    useEffect(() => {
+        scroller.scrollTo(link, {
+            duration: 800,
+            delay: 0,
+            smooth: "easeInOutQuart"
+        });
+    }, []);
 
     return (
         <StyledLandingPage>
