bd7215a532a25a27a31548b5be202b19952059d5
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-02 22:21:09 +0200

    Added picture and description imports for the entree project

---
+++ src/static/util/entreeimgLoader.js
@@ -0,0 +1,20 @@
+import Img1 from "../img/entree/001_DSC0329.jpg";
+import Img2 from "../img/entree/002_DSC0500.jpg";
+import Img3 from "../img/entree/003_DSC0305.jpg";
+import Img4 from "../img/entree/004_DSC0318.jpg";
+import Img5 from "../img/entree/005_DSC0615.jpg";
+import Img6 from "../img/entree/006_DSC0206.jpg";
+import Img7 from "../img/entree/007_DSC0269.jpg";
+
+export const entreeImages = [
+    { id: 1, src: Img1 },
+    { id: 2, src: Img2 },
+    { id: 3, src: Img3 },
+    { id: 4, src: Img4 },
+    { id: 5, src: Img5 },
+    { id: 6, src: Img6 },
+    { id: 7, src: Img7 }
+];
+
+export const entreeDesc =
+    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...";
