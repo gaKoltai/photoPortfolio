e55c727fe0b9a25f5045464e5f56a3b53f888fc7
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-09 09:35:33 +0200

    Changed router

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Footer from "./components/footer/Footer";
 
 function App() {
     return (
-        <Router>
+        <Router basename="/">
             <NavigationContextProvider>
                 <NavBar />
                 <Switch>
